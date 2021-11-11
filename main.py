890e0d213071484322604db656a88a42bf93c7f9
lnrsx <felixanderton5@gmail.com>
2021-11-11 09:00:46 +0000

    cleaned up repetetive code

--- main.py
+++ main.py
@@ -15,66 +15,44 @@ time_mode = True
 list_unsorted = [i for i in range(100)]
 random.shuffle(list_unsorted)
 
-def begin_bubble(visual = True):
-    start_time = time()
-    random.shuffle(list_unsorted)
-    if visual:
-        draw_list(list_unsorted, window, canvas, done = False)
-    sort_functions.bubble(list_unsorted, window = window if visual else None, canvas = canvas if visual else None)
-    if visual:
-        draw_list(list_unsorted, window, canvas, done = True)
-    return time() - start_time
+sort_types = {
+    "Bubble": sort_functions.bubble,
+    "Insert": sort_functions.insert,
+    "Quick": sort_functions.quick,
+    "Merge": sort_functions.merge
+}
 
-def begin_insert(visual = True):
+def begin_sort(type, visual = True):
     start_time = time()
     random.shuffle(list_unsorted)
     if visual:
         draw_list(list_unsorted, window, canvas, done = False)
-    sort_functions.insert(list_unsorted, window = window if visual else None, canvas = canvas if visual else None)
-    if visual:
-        draw_list(list_unsorted, window, canvas, done = True)
-    return time() - start_time
-
-def begin_quick(visual = True):
-    start_time = time()
-    random.shuffle(list_unsorted)
-    if visual:
-        draw_list(list_unsorted, window, canvas, done = False)
-    sort_functions.quick(list_unsorted, 0, len(list_unsorted) - 1, window = window if visual else None, canvas = canvas if visual else None)
-    if visual:
-        draw_list(list_unsorted, window, canvas, done = True)
-    return time() - start_time
-
-def begin_merge(visual = True):
-    start_time = time()
-    random.shuffle(list_unsorted)
-    if visual:
-        draw_list(list_unsorted, window, canvas, done = False)
-    sort_functions.merge(list_unsorted, 0, len(list_unsorted) - 1, window = window if visual else None, canvas = canvas if visual else None)
+    sort_types[type](list_unsorted, window = window if visual else None, canvas = canvas if visual else None, low = 0, high = len(list_unsorted) - 1)
     if visual:
         draw_list(list_unsorted, window, canvas, done = True)
     return time() - start_time
 
 if time_mode:
-    bubble, insert, quick, merge = [], [], [], []
+    sort_data = {
+        "Bubble": [],
+        "Insert": [],
+        "Quick": [],
+        "Merge": []
+    }
     list_range = np.arange(100, 1001, 10)
     for list_len in list_range:
         list_unsorted = [i for i in range(list_len)]
-        random.shuffle(list_unsorted)
-        bubble.append(begin_bubble(visual=False) * 1000)
-        insert.append(begin_insert(visual=False) * 1000)
-        quick.append(begin_quick(visual=False) * 1000)
-        merge.append(begin_merge(visual=False) * 1000)
+
+        for method, time_list in sort_data.items():
+            time_list.append(begin_sort(method, visual=False) * 1000)
 
         print(f"Plotted list size {list_len} data")
     
-    plt.scatter(np.array(bubble), list_range, label = "Bubble Sort", s = 10)
-    plt.scatter(np.array(insert), list_range, label = "Insert Sort", s = 10)
-    plt.scatter(np.array(quick), list_range, label = "Quick Sort", s = 10)
-    plt.scatter(np.array(merge), list_range, label = "Merge Sort", s = 10)
+    for method, time_list in sort_data.items():
+        plt.scatter( list_range, np.array(time_list), label = f"{method} Sort", s = 10)
     
-    plt.xlabel("Time taken to sort (ms)")
-    plt.ylabel("List size")
+    plt.xlabel("List Size")
+    plt.ylabel("Time taken to sort (ms)")
     plt.title("Sorting Algorithm Comparison")
     plt.legend()
     plt.show()
@@ -87,14 +65,12 @@ else:
     UI_frame = tkinter.Frame(window, width= 900, height=300, bg=white)
     UI_frame.grid(row=0, column=0, padx=10, pady=5)
 
-    sort_methods = [begin_bubble, begin_insert, begin_quick, begin_merge]
-    
-    for index, method in enumerate(['bubble', 'insert', 'quick', 'merge']):
-        button = tkinter.Button(UI_frame, text=method, command=sort_methods[index], bg=light_gray)
-        button.grid(row=1, column=index, padx=5, pady=5)
-
     canvas = tkinter.Canvas(window, width = 800, height = 400, bg = light_gray)
     canvas.grid(row = 1, column= 0, padx = 10, pady = 5)
+    
+    for index, (method, function) in enumerate(sort_types.items()):
+        button = tkinter.Button(UI_frame, text=method, command=lambda m=method: begin_sort(m), bg=light_gray)
+        button.grid(row=1, column=index, padx=5, pady=5)
 
     draw_list(list_unsorted, window, canvas)
     window.mainloop()
--- sort_functions.py
+++ sort_functions.py
@@ -4,7 +4,7 @@ from draw_functions import draw_list
 from utils import *
 
 
-def bubble(list, window = None, canvas = None):
+def bubble(list, window = None, canvas = None, low = None, high = None):
     size = len(list)
     for i in range(size-1):
         for j in range(size-i-1):
@@ -14,7 +14,7 @@ def bubble(list, window = None, canvas = None):
                     draw_list(list, window, canvas, pointer = j + 1)
 
 
-def insert(list, window = None, canvas = None):
+def insert(list, window = None, canvas = None, low = None, high = None):
     for i in range(1, len(list)):
         key = list[i]
         j = i-1
@@ -26,71 +26,76 @@ def insert(list, window = None, canvas = None):
         list[j+1] = key 
 
 
-def quicksort_partition(list, low, high):
-   i = ( low-1 )
-   pivot = list[high]
-   for j in range(low , high):
-      if list[j] <= pivot:
-         i = i+1
-         list[i],list[j] = list[j],list[i]
-   list[i+1],list[high] = list[high],list[i+1]
-   return ( i+1 )
+def quicksort_partition(list, low, high, window = None, canvas = None):
+    i = ( low-1 )
+    pivot = list[high]
+    for j in range(low , high):
+        if list[j] <= pivot:
+            i = i+1
+            if window and canvas:
+                draw_list(list, window, canvas, pointer = i)
+            list[i],list[j] = list[j],list[i]
+    list[i+1],list[high] = list[high],list[i+1]
+    if window and canvas:
+        draw_list(list, window, canvas, pointer = i)
+    return ( i+1 )
 
-def quick(list, low, high, window = None, canvas = None):
+def quick(list, low = 0, high = 0, window = None, canvas = None):
     if low < high:
-        pi = quicksort_partition(list, low, high)
-        quick(list, low, pi-1, window, canvas)
-        quick(list, pi+1, high, window, canvas)
+        pi = quicksort_partition(list, low, high, window = window, canvas = canvas)
         if window and canvas:
             draw_list(list, window, canvas, pointer = pi)
+        quick(list, window = window, canvas = canvas, low = low, high = pi-1)
+        quick(list, window = window, canvas = canvas, low = pi+1, high = high)
 
 
-def merge_lists(arr, l, m, r, window = None, canvas = None):
-    n1 = m - l + 1
-    n2 = r - m
+def merge_lists(arr, left_pointer, mid_point, right_pointer, window = None, canvas = None):
+    n1 = mid_point - left_pointer + 1
+    n2 = right_pointer - mid_point
 
     L = [0] * (n1)
     R = [0] * (n2)
 
     for i in range(0, n1):
-        L[i] = arr[l + i]
+        L[i] = arr[left_pointer + i]
  
     for j in range(0, n2):
-        R[j] = arr[m + 1 + j]
+        R[j] = arr[mid_point + 1 + j]
  
     i = 0
     j = 0
-    k = l
+    pointer = left_pointer
     if window and canvas:
-        draw_list(arr, window, canvas, pointer = k + 1)
+        draw_list(arr, window, canvas, pointer = pointer + 1)
 
     while i < n1 and j < n2:
         if L[i] <= R[j]:
-            arr[k] = L[i]
+            arr[pointer] = L[i]
             i += 1
         else:
-            arr[k] = R[j]
+            arr[pointer] = R[j]
             j += 1
-        k += 1
+        pointer += 1
         if window and canvas:
-            draw_list(arr, window, canvas, pointer = k + 1)
+            draw_list(arr, window, canvas, pointer = pointer + 1)
 
     while i < n1:
-        arr[k] = L[i]
+        arr[pointer] = L[i]
         i += 1
-        k += 1
+        pointer += 1
 
     while j < n2:
-        arr[k] = R[j]
+        arr[pointer] = R[j]
         j += 1
-        k += 1
- 
+        pointer += 1
 
-def merge(arr, l, r, window = None, canvas = None):
-    if l < r:
-        m = l+(r-l)//2
+
+def merge(arr, low = 0, high = 0, window = None, canvas = None):
+    left_pointer, right_pointer = low, high
+    if left_pointer < right_pointer:
+        mid_point = left_pointer + (right_pointer - left_pointer) // 2
  
-        merge(arr, l, m, window = window, canvas = canvas)
-        merge(arr, m+1, r, window = window, canvas = canvas)
-        merge_lists(arr, l, m, r, window = window, canvas = canvas)
+        merge(arr, left_pointer, mid_point, window = window, canvas = canvas)
+        merge(arr, mid_point+1, right_pointer, window = window, canvas = canvas)
+        merge_lists(arr, left_pointer, mid_point, right_pointer, window = window, canvas = canvas)
  
